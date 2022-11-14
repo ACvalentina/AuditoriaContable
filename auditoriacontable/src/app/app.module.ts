2bcbb4c022ddca25bb121fa23898d5bd5dd692f7
ACvalentina <118214058+ACvalentina@users.noreply.github.com>
2022-11-14 18:47:58 -0300

    inicio

--- auditoriacontable/src/app/app.module.ts
+++ auditoriacontable/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
