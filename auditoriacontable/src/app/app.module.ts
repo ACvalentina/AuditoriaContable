d67f276e846a1c1632b7d1aee9dfc33cc0910821
ACvalentina <118214058+ACvalentina@users.noreply.github.com>
2022-11-16 12:28:23 -0300

    enlace a bbdd firestore

--- auditoriacontable/src/app/app.module.ts
+++ auditoriacontable/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { Inicio2Component } from './inicio2/inicio2.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 const appRoutes : Routes = [
   {path:'',component:Inicio2Component},
@@ -34,7 +34,7 @@ const appRoutes : Routes = [
     RouterModule.forRoot(appRoutes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase())
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
