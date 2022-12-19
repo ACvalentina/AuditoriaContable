b474513c229fbb6e7272faa1c67939a70d4857bd
ACvalentina <118214058+ACvalentina@users.noreply.github.com>
2022-12-19 11:24:48 -0300

    solución errores

--- auditoriacontable/src/app/contabilidad-compras/contabilidad-compras.component.ts
+++ auditoriacontable/src/app/contabilidad-compras/contabilidad-compras.component.ts
@@ -6,7 +6,6 @@ import * as XLSX from 'xlsx';
 import { CuentasService } from '../cuentas.service'
 import { BehaviorSubject, Observable } from 'rxjs'
 import { isEmpty } from '@firebase/util';
-import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contabilidad-compras',
@@ -70,82 +69,7 @@ export class ContabilidadComprasComponent implements OnInit {
     }
     //if(nombreCuenta === isEmpty){ nombreCuenta = '' }
   }
-  async Guardar(){
-    if(this.comprobarCodigosDoc()===true){
-      Swal.fire({
-        title: '¡Guardado!',
-        text: 'Se ha guardado una nueva compra',
-        icon: 'success',
-        allowOutsideClick: false,
-      })
-      const ref = collection(this.firestore,'Contabilidad-Compras');
-      const id = await this.getUid();//arreglar los guardados
 
-      for(let i = 1; i<this.excelData.length ; i++){
-        
-        for(let j = 0; j<29 ; j++){
-          if(this.excelData[i][j]===undefined){
-            this.excelData[i][j] = ''
-          }
-          if(this.nombreCuenta[i]===undefined){
-            this.nombreCuenta[i] = ''
-          }
-          
-        }
-        let obj = Object.assign({
-        "UID":id,
-        "Mes": this.addMes,
-        "Año": this.addAnio,
-        "Cuenta": this.nombreCuenta[i],
-        "Nro":this.excelData[i][0],
-        "Tipo Doc":this.excelData[i][1],
-        "Tipo Compra":this.excelData[i][2],
-        "Rut Proveedor":this.excelData[i][3],
-        "Razon Social":this.excelData[i][4],
-        "Folio":this.excelData[i][5],
-        "Fecha Docto":this.excelData[i][6],  //arreglar formato fechas
-        "Fecha Recepcion":this.excelData[i][7], //arreglas formato fechas
-        "Fecha Acuse":this.excelData[i][8],
-        "Monto Exento":this.excelData[i][9],
-        "Monto Neto":this.excelData[i][10],
-        "Monto IVA Recuperable":this.excelData[i][11],
-        "Monto Iva No Recuperable":this.excelData[i][12],
-        "Codigo IVA No Rec.":this.excelData[i][13],
-        "Monto Total":this.excelData[i][14],
-        "Monto Neto Activo Fijo":this.excelData[i][15],
-        "IVA Activo Fijo":this.excelData[i][16],
-        "IVA uso Comun":this.excelData[i][17],
-        "Impto. Sin Derecho a Credito":this.excelData[i][18],
-        "IVA No Retenido":this.excelData[i][19],
-        "Tabacos Puros":this.excelData[i][20],
-        "Tabacos Cigarrillos":this.excelData[i][21],
-        "Tabacos Elaborados":this.excelData[i][22],
-        "NCE o NDE sobre Fact. de Compra":this.excelData[i][23],
-        "Codigo Otro Impuesto":this.excelData[i][24],
-        "Valor Otro Impuesto":this.excelData[i][25],
-        "Tasa Otro Impuesto":this.excelData[i][26]
-        })
-        console.log(obj)
-        if(this.addMes === '' || this.addMes === '0' || this.addAnio === '' || this.addAnio === '0'){
-          Swal.fire({
-            title: '¡Cuidado!',
-            text: 'Debes seleccionar mes y año',
-            icon: 'warning',
-            allowOutsideClick: false,
-          })
-        }else{
-          addDoc(ref,obj)
-        }
-         //corroborar si ya esta en la bd?
-        //segun tipo documento, realizar una funcion
-  cuentasToString(){
-    for(let i = 0; i< this.nombreCuenta.length; i++){
-      if(this.nombreCuenta[i]===undefined){
-        this.nombreCuenta[i]=''
-      }
-    }
-    //if(nombreCuenta === isEmpty){ nombreCuenta = '' }
-  }
   async Guardar(){
     if(this.comprobarCodigosDoc()===true){
       this.cuentasToString()
@@ -171,6 +95,8 @@ export class ContabilidadComprasComponent implements OnInit {
           }
           let obj = Object.assign({
           "UID":id,
+          "Mes": this.addMes,
+          "Año": this.addAnio,
           "Cuenta": this.nombreCuenta[i],
           "Nro":this.excelData[i][0],
           "Tipo Doc":this.excelData[i][1],
@@ -200,8 +126,18 @@ export class ContabilidadComprasComponent implements OnInit {
           "Valor Otro Impuesto":this.excelData[i][25],
           "Tasa Otro Impuesto":this.excelData[i][26]
           })
+          if(this.addMes === '' || this.addMes === '0' || this.addAnio === '' || this.addAnio === '0'){
+            Swal.fire({
+              title: '¡Cuidado!',
+              text: 'Debes seleccionar mes y año',
+              icon: 'warning',
+              allowOutsideClick: false,
+            })
+          }else{
+            addDoc(ref,obj)
+          }
           //console.log(obj)
-          addDoc(ref,obj) //corroborar si ya esta en la bd?
+           //corroborar si ya esta en la bd?
           //segun tipo documento, realizar una funcion
         }
       }
