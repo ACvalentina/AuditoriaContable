6c9ccd8c0cda883d2152dd8c25443e8ba6069215
EdenFly66 <yerkoandresespinozasoto@gmail.com>
2022-12-19 11:30:15 -0300

    cambio funciones

--- auditoriacontable/src/app/contabilidad-compras/contabilidad-compras.component.ts
+++ auditoriacontable/src/app/contabilidad-compras/contabilidad-compras.component.ts
@@ -5,7 +5,6 @@ import Swal from 'sweetalert2';
 import * as XLSX from 'xlsx'; 
 import { CuentasService } from '../cuentas.service'
 import { BehaviorSubject, Observable } from 'rxjs'
-import { isEmpty } from '@firebase/util';
 
 @Component({
   selector: 'app-contabilidad-compras',
@@ -16,6 +15,8 @@ import { isEmpty } from '@firebase/util';
 
 export class ContabilidadComprasComponent implements OnInit {
   excelData: Array<any>=[] as any;
+  activosFijos: Array<any>=[] as any;
+  objetos: Array<any>=[] as any;
   public show = false;
   cuentas$: Observable<any[]>;
   startAt: BehaviorSubject<string> = new BehaviorSubject('');
@@ -69,8 +70,12 @@ export class ContabilidadComprasComponent implements OnInit {
     }
     //if(nombreCuenta === isEmpty){ nombreCuenta = '' }
   }
+<<<<<<< Updated upstream
 
-  async Guardar(){
+  
+=======
+  async agregarDetalles(){
+>>>>>>> Stashed changes
     if(this.comprobarCodigosDoc()===true){
       this.cuentasToString()
       if(this.revisarCuentas()){
@@ -93,6 +98,7 @@ export class ContabilidadComprasComponent implements OnInit {
               this.nombreCuenta[i] = ''
             }
           }
+<<<<<<< Updated upstream
           let obj = Object.assign({
           "UID":id,
           "Mes": this.addMes,
@@ -140,6 +146,47 @@ export class ContabilidadComprasComponent implements OnInit {
            //corroborar si ya esta en la bd?
           //segun tipo documento, realizar una funcion
         }
+=======
+          if(!this.comprobarActivoFijo(this.nombreCuenta[i])){
+            let obj = Object.assign({
+              "UID":id,
+              "Cuenta": this.nombreCuenta[i],
+              "Nro":this.excelData[i][0],
+              "Tipo Doc":this.excelData[i][1],
+              "Tipo Compra":this.excelData[i][2],
+              "Rut Proveedor":this.excelData[i][3],
+              "Razon Social":this.excelData[i][4],
+              "Folio":this.excelData[i][5],
+              "Fecha Docto":this.excelData[i][6],  //arreglar formato fechas
+              "Fecha Recepcion":this.excelData[i][7], //arreglas formato fechas
+              "Fecha Acuse":this.excelData[i][8],
+              "Monto Exento":this.excelData[i][9],
+              "Monto Neto":this.excelData[i][10],
+              "Monto IVA Recuperable":this.excelData[i][11],
+              "Monto Iva No Recuperable":this.excelData[i][12],
+              "Codigo IVA No Rec.":this.excelData[i][13],
+              "Monto Total":this.excelData[i][14],
+              "Monto Neto Activo Fijo":this.excelData[i][15],
+              "IVA Activo Fijo":this.excelData[i][16],
+              "IVA uso Comun":this.excelData[i][17],
+              "Impto. Sin Derecho a Credito":this.excelData[i][18],
+              "IVA No Retenido":this.excelData[i][19],
+              "Tabacos Puros":this.excelData[i][20],
+              "Tabacos Cigarrillos":this.excelData[i][21],
+              "Tabacos Elaborados":this.excelData[i][22],
+              "NCE o NDE sobre Fact. de Compra":this.excelData[i][23],
+              "Codigo Otro Impuesto":this.excelData[i][24],
+              "Valor Otro Impuesto":this.excelData[i][25],
+              "Tasa Otro Impuesto":this.excelData[i][26]
+              })
+              //console.log(obj)
+              addDoc(ref,obj) //corroborar si ya esta en la bd?
+              //segun tipo documento, realizar una funcion
+              }
+              //guardar nueva tabla
+          }
+          
+>>>>>>> Stashed changes
       }
       else{
         Swal.fire({
@@ -192,7 +239,7 @@ export class ContabilidadComprasComponent implements OnInit {
     this.startAt.next(searchText)
   }
 
-  revisarCuentas(){///focus aca
+  revisarCuentas(){///seguro tabla
     let cont=0
     console.log(cont)
     for(let i = 0; i<this.excelData.length; i++){
@@ -205,6 +252,23 @@ export class ContabilidadComprasComponent implements OnInit {
     return true
   }
   //FUNCIÓN ACTIVO FIJO
+<<<<<<< Updated upstream
  
 
+=======
+  comprobarActivoFijo(cuenta:string){
+    let tipoCuenta = cuenta.substring(0,2)
+    console.log(tipoCuenta)
+    if(tipoCuenta==="12"){
+      console.log('sisi')
+      return true 
+    }
+    return false
+    
+  }
+  
+  Guardar(){
+    
+  }
+>>>>>>> Stashed changes
 }
